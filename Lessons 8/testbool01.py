a37f0872094ba7143c27d0dedcdbb7d0e117041c
200202iqbal <75594145+200202iqbal@users.noreply.github.com>
2021-04-30 09:01:04 +0900

    lessons

---
+++ Lessons 8/testbool01.py
@@ -0,0 +1,3 @@
+moji1, moji2,moji3 = "","a","b"
+non_null = moji1 or moji2 or moji3
+print(non_null)
---
+++ Lessons 8/testbool02.py
@@ -0,0 +1,4 @@
+print("ABC"<"C"<"Pascal","Python")
+print((1,2,3) < (1,2,4))
+print([1, 2, 3]              < [1, 2, 4])
+print((1, 2, ('aa', 'ab'))   < (1, 2, ('abc', 'a'), 4))
