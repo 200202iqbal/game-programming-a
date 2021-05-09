a8e64bb6ca41170a28262141b5691cb1beb2c8a5
200202iqbal <200202yr@yse-c.net>
2021-05-09 18:14:57 +0900

    golden week

---
+++ GoldenWeek/calc001.py
@@ -0,0 +1,10 @@
+try:
+    number = int(input("Please enter any number : "))
+    print(number+7)
+    print(number-7)
+    print(number*7)
+    print(number/7)
+    print(number//7)
+    print(number%7)
+except:
+    print("----- Please enter a number ------")
---
+++ GoldenWeek/loop001.py
@@ -0,0 +1,5 @@
+words = input("Please enter an word : ")
+iteration = 0
+while iteration < len(words):
+    iteration+=1
+    print(words)
