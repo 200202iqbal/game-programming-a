39ef050c0f0b7f05f76cff0b40cac118ed25ef25
200202iqbal <75594145+200202iqbal@users.noreply.github.com>
2021-05-19 14:22:08 +0900

    python

---
+++ Error/testerror01.py
@@ -0,0 +1,6 @@
+#while True print("Hello world") #test error sintaks
+
+#x  = 10 * (1/0) #ゼロ割 / pembagian 0
+spam = 5
+x = 4 + spam * 3
+y = "2" + 2
--- Lessons 8/testfile04.py
+++ Lessons 8/testfile04.py
@@ -1,2 +1,6 @@
 with open("workfile03.txt", "rb+") as f:
-    f.write(b"0123456789abcdef")
+    f.write(b"01234567sdfsdfsdffsdfsdfsd89abcdef")
+    f.seek(5)   #go to 6th byte in the file
+    f.read(1)
+    f.seek(-3,2) #go to the 3rd byte before the end
+    f.read(1)
