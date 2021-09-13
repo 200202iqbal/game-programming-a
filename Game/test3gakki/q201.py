aa7768cb820a78f48d962f115b7483cf5dc9976f
200202iqbal <200202yr@yse-c.net>
2021-09-13 13:05:31 +0900

    Game Python

--- Game/test3gakki/q201.py
+++ Game/test3gakki/q201.py
@@ -1,23 +1,21 @@
-### fukushu3.py ###ß
+### 学生番号 : 200202
+### 名前 : ムハンマドイクバルタワカル
+
 import pygame
 import sys
 from pygame.locals import QUIT
-import random
 
 pygame.init()
-SURFACE  =pygame.display.set_mode((400,300))
+SURFACE = pygame.display.set_mode((600,600))
 FPSCLOCK = pygame.time.Clock()
 
 def main():
-    x = random.randint(0,300)
-    y = random.randint(0,200)
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
         SURFACE.fill((255,255,255))
-        pygame.draw.rect(SURFACE,(255,0,0),(x,y,100,100))
 
         pygame.display.update()
         FPSCLOCK.tick(30)
--- Game/test3gakki/q202.py
+++ Game/test3gakki/q202.py
@@ -1,24 +1,25 @@
-### fukushu3.py ###ß
+### 学生番号 : 200202
+### 名前 : ムハンマドイクバルタワカル
+
 import pygame
 import sys
 from pygame.locals import QUIT
-import random
 
 pygame.init()
-SURFACE  =pygame.display.set_mode((400,300))
+SURFACE = pygame.display.set_mode((600,600))
 FPSCLOCK = pygame.time.Clock()
 
 def main():
-    x = random.randint(0,300)
-    y = random.randint(0,200)
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
         SURFACE.fill((255,255,255))
-        pygame.draw.rect(SURFACE,(255,0,0),(x,y,100,100))
-
+        for y in range(3):
+            for x in range(3):
+                pygame.draw.circle(SURFACE,(0,255,0),(x * 200 + 100, y * 200 + 100),50)
+                
         pygame.display.update()
         FPSCLOCK.tick(30)
 
